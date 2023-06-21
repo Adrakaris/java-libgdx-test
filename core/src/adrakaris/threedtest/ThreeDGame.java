ab531f066da792c4dea0d913dcec094b498749a4
Yijun Hu <huyijun007@gmail.com>
2023-06-21 14:20:10 +0100

    Cube with lichtung

--- core/src/adrakaris/threedtest/ThreeDGame.java
+++ core/src/adrakaris/threedtest/ThreeDGame.java
@@ -1,31 +1,83 @@
 package adrakaris.threedtest;
 
 import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.*;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g3d.*;
+import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
+import com.badlogic.gdx.graphics.g3d.environment.DirectionalLight;
+import com.badlogic.gdx.graphics.g3d.utils.CameraInputController;
+import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class ThreeDGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
+
+	public PerspectiveCamera camera;
+	public CameraInputController camController;
+	public ModelBatch modelBatch;
+	public Environment environment;
+
+	public Model model;
+	public ModelInstance instance;
+
 	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+	public void create() {
+		modelBatch = new ModelBatch();
+
+		// setup camera
+		camera = new PerspectiveCamera(67, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		camera.position.set(10, 10, 10);
+		camera.lookAt(0,0,0);
+		camera.near = 1;
+		camera.far = 300;
+		camera.update();
+
+		camController = new CameraInputController(camera);
+		Gdx.input.setInputProcessor(camController);
+
+		// evn
+		environment = new Environment();
+		environment.set(new ColorAttribute(ColorAttribute.AmbientLight, .4f, .4f, .4f, 1));
+		environment.add(new DirectionalLight().set(.8f, .8f, .8f, -1, -.8f, -.2f));
+
+
+
+		// cube
+		ModelBuilder modelBuilder = new ModelBuilder();
+		model = modelBuilder.createBox(
+				5,5,5,
+				new Material(ColorAttribute.createDiffuse(Color.GREEN)),
+				VertexAttributes.Usage.Position | VertexAttributes.Usage.Normal);
+
+		instance = new ModelInstance(model);
 	}
 
 	@Override
-	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+	public void resize(int width, int height) {
 	}
-	
+
 	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
+	public void render() {
+		Gdx.gl.glViewport(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
+
+		modelBatch.begin(camera);
+		modelBatch.render(instance, environment);
+		modelBatch.end();
+	}
+
+	@Override
+	public void pause() {
+	}
+
+	@Override
+	public void resume() {
+	}
+
+	@Override
+	public void dispose() {
+		modelBatch.dispose();
+		model.dispose();
 	}
 }
